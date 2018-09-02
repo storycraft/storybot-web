35c1e36554948abc3421ae9c77022665ae21106b
storycraft <dbsxoal123@naver.com>
2018-09-02 14:49:37 +0900

    first commit

---
+++ client.js
@@ -0,0 +1,57 @@
+var box = null;
+var stdin = null;
+var btn = null;
+var manager = null;
+
+const initData = {
+    "namespace": "test",
+    "service": "testing mode",
+    "id": -282
+};
+
+window.onload = () => {
+    box = document.getElementById('field');
+    stdin = document.getElementById('input');
+    btn = document.getElementById('btn');
+    manager = io.connect('http://bot.storyboard.ml:7000', {
+        path: '/storybot-web',
+        forceNew: true
+    });
+
+    manager.on('connect', () => {
+        console.log('connected');
+        manager.emit('initialize', initData);
+    });
+
+    manager.on('message', (data) => {
+        var a = document.createElement('p');
+
+        a.appendChild(document.createTextNode(data.user.nickname + ': ' + data.text));
+
+        box.appendChild(a);
+    });
+
+    btn.addEventListener('click', () => {
+        manager.emit('message', {
+            "channel": {
+                "id": 182739123,
+                "name": "testing"
+            },
+            "user": {
+                "id": 178293671283,
+                "nickname": "tester" 
+            },
+            "message": {
+                "timestamp": new Date,
+                "attachments": [],
+                "text": stdin.value
+            }
+        });
+
+        var a = document.createElement('p');
+
+        a.appendChild(document.createTextNode('you: ' + stdin.value));
+        
+        box.appendChild(a);
+    });
+}
